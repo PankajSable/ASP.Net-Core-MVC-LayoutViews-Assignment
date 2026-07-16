e3e1e1e18b4886341f307f3f17e8166797febf88
Pankaj Sable <86041481+PankajSable@users.noreply.github.com>
2026-07-16 17:24:15 +0530

    Add files via upload

---
+++ MVCViewsAssignment/MVCViewsAssignment/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCViewsAssignment.Models;
+
+namespace MVCViewsAssignment.Controllers
+{
+    public class WeatherController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<CityWeather> cityWeathers = new List<CityWeather>
+            {
+                new CityWeather { CityUniqeCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"),  TemperatureFahrenheit = 33, humidity = 50, windSpeed = 15, weather = "Clouds" },
+                new CityWeather { CityUniqeCode = "NYC", CityName = "New York", DateAndTime = DateTime.Parse("2030-01-01 3:00"), TemperatureFahrenheit = 60, humidity = 60, windSpeed = 20, weather = "Clear" },
+                new CityWeather { CityUniqeCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"), TemperatureFahrenheit = 82, humidity = 70, windSpeed = 25, weather = "Rain" }
+            };
+            return View(cityWeathers);
+        }
+
+        [Route("/Weather/{cityCode}")]
+        public IActionResult WeatherWithCity(string cityCode)
+        {
+            List<CityWeather> cityWeathers = new List<CityWeather>
+            {
+                new CityWeather { CityUniqeCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"),  TemperatureFahrenheit = 33, humidity = 50, windSpeed = 15, weather = "Clouds" },
+                new CityWeather { CityUniqeCode = "NYC", CityName = "New York", DateAndTime = DateTime.Parse("2030-01-01 3:00"), TemperatureFahrenheit = 60, humidity = 60, windSpeed = 20, weather = "Clear" },
+                new CityWeather { CityUniqeCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"), TemperatureFahrenheit = 82, humidity = 70, windSpeed = 25, weather = "Rain" }
+            };
+            
+            CityWeather? cityWeather = cityWeathers.FirstOrDefault(c => c.CityUniqeCode == cityCode.ToUpper());
+            return View(cityWeather);
+        }
+    }
+}
---
+++ MVCViewsAssignment/MVCViewsAssignment/Models/CityWeather.cs
@@ -0,0 +1,16 @@
+﻿namespace MVCViewsAssignment.Models
+{
+    public class CityWeather
+    {
+        public string? CityUniqeCode { get; set; }
+        public string? CityName { get; set; }
+
+        public DateTime? DateAndTime { get; set; }
+
+        public int? TemperatureFahrenheit { get; set; }
+        public int? humidity { get; set; }
+        public int? windSpeed { get; set; }
+
+        public string weather { get; set;  }
+}
+}
